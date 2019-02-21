6b5a00436714ebf79c1b013dd122bbfa3f95221d
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2019-02-21 11:31:41 -0300

    Remove unusual exceptions

--- scrooge/exception.py
+++ scrooge/exception.py
@@ -10,17 +10,3 @@ class ScroogeClientException(Exception):
     This exceptions is used only in Client module
     """
     pass
-
-
-class ScroogeRedisCacheException(Exception):
-    """
-    This exceptions is used only in Redis Client
-    """
-    pass
-
-
-class ScroogeMemcacheCacheException(Exception):
-    """
-    This exceptions is used only in Memcache Client
-    """
-    pass

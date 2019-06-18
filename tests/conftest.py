ef9af0eec758389cf1f263cdf6940dedecbcfafa
Marcelo Moraes <marcelo.moraes@geru.com.br>
2019-06-18 19:52:22 -0300

    Fix Conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,44 +63,44 @@ def r_tuple_delay(delay_time=5):
     return (1, 2)
 
 
-@memcache_client.gentlemen_cache(namespace='dict_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='dict_delay_m', expiration_time=2)
 def m_dict_delay(delay_time=5, delay_time_=2):
     time.sleep(delay_time + delay_time_)
     print("Delay finished")
     return {delay_time: delay_time_}
 
 
-@memcache_client.gentlemen_cache(namespace='obj_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='obj_delay_m', expiration_time=2)
 def m_obj_delay(delay_time=5):
     return Test()
 
 
-@memcache_client.gentlemen_cache(namespace='str_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='str_delay_m', expiration_time=2)
 def m_str_delay(delay_time=1):
     return 'foo-bar'
 
 
-@memcache_client.gentlemen_cache(namespace='int_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='int_delay_m', expiration_time=2)
 def m_int_delay(delay_time=2):
     return 9
 
 
-@memcache_client.gentlemen_cache(namespace='float_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='float_delay_m', expiration_time=2)
 def m_float_delay(delay_time=3):
     return 1.4
 
 
-@memcache_client.gentlemen_cache(namespace='list_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='list_delay_m', expiration_time=2)
 def m_list_delay(delay_time=4):
     return ['foo', 'bar']
 
 
-@memcache_client.gentlemen_cache(namespace='tuple_delay', expiration_time=2)
+@memcache_client.gentlemen_cache(namespace='tuple_delay_m', expiration_time=2)
 def m_tuple_delay(delay_time=5):
     return (1, 2)
 
 
-@memcache_client.gentlemen_cache(namespace='no_cache', expiration_time=10)
+@memcache_client.gentlemen_cache(namespace='no_cache_m', expiration_time=10)
 def m_no_cache(**kwargs):
     time.sleep(2)
     return "Scrooge got you!!"
